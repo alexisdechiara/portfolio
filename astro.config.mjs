d5b032d5ec7a713193e84944174c30dc41c16dae
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2022-09-28 17:08:40 +0200

    Version 0.5.1
    Remplacement de edge-functions par functions pour netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
