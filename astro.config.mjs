c5c780733c6b52b8568c7d67f95eaf354395a19e
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2022-09-28 17:00:25 +0200

    Version 0.5.0
    Ajout de netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/edge-functions";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [astroI18next(), tailwind({ config: { applyBaseStyles: false } }), image()],
+	output: "server",
+	adapter: netlify(),
 });
