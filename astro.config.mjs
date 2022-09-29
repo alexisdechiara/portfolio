6ac4da8475e0ab4ead72b6b984025fd333223c22
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2022-09-29 11:38:38 +0200

    publication sur deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
+
+//import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [astroI18next(), tailwind({ config: { applyBaseStyles: false } }), image()],
 	output: "server",
-	adapter: netlify(),
+	adapter: vercel(),
 });
