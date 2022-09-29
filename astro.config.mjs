bffee90f36ff7d10e359d80c7c3829d1dfccd982
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2022-09-29 14:45:31 +0200

    Test de correction avec netlify.toml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import image from "@astrojs/image";
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 
-//import netlify from "@astrojs/netlify/functions";
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [astroI18next(), tailwind({ config: { applyBaseStyles: false } }), image()],
 	output: "server",
-	adapter: vercel(),
+	adapter: netlify(),
 });
