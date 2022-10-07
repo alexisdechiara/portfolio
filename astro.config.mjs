56da8cfb825eed5a5d0196ea31f0fe25b26d0e42
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2022-10-07 11:46:43 +0200

    Ajout de sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@ import image from "@astrojs/image";
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 
-// import netlify from "@astrojs/netlify/functions";
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [astroI18next(), tailwind({ config: { applyBaseStyles: false } }), image()],
+	integrations: [
+		astroI18next(),
+		tailwind({ config: { applyBaseStyles: false } }),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+	],
 	output: "server",
-	adapter: vercel(),
+	adapter: netlify(),
 });
