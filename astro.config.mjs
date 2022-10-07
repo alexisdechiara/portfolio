9512c0585507292a8b6fa1a116298d5cfd0c0b10
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2022-10-07 15:31:35 +0200

    Type module package

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify/functions";
 // import vercel from "@astrojs/vercel/serverless";
+// import node from "@astrojs/node"
 
 // https://astro.build/config
 export default defineConfig({
