5b1dd7d1616a3b58fec1ed6a58b59dc50e6ad6c1
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2023-01-06 13:51:18 +0100

    Version 0.6.2 : Bug fixe + compression + SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
-
 import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://alexisdechiara.fr",
+	site: "https://alexisdechiara.geekly.blog",
 	integrations: [
 		tailwind({
 			config: {
@@ -19,5 +19,6 @@ export default defineConfig({
 		}),
 		partytown(),
 		sitemap(),
+		compress(),
 	],
 });
