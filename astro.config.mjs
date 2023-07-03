fdb91f182adbca1390b3458bd4470d792aee9b9d
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2023-07-03 14:37:09 +0200

    Version 0.8.0 : Mise à jour d'Astro et ajout de Storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,23 @@ import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://alexisdechiara.geekly.blog",
-	integrations: [
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		image({
-			serviceEntryPoint: "@astrojs/image/sharp",
-		}),
-		partytown(),
-		sitemap(),
-		compress(),
-	],
+	integrations: [tailwind({
+		applyBaseStyles: false
+	}), image({
+		serviceEntryPoint: "@astrojs/image/sharp"
+	}),
+		storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+      },
+    }), partytown(), sitemap(), compress()],
 });
