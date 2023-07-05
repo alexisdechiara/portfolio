879c0da87933f3ed1b5bbb830cc0672c39216df4
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2023-07-05 10:49:51 +0200

    Mise en place des composants storyblok - part. 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
 
@@ -17,10 +18,25 @@ export default defineConfig({
 	}), image({
 		serviceEntryPoint: "@astrojs/image/sharp"
 	}),
-		storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        // Add your components here
-      },
+	storyblok({
+    	accessToken: env.STORYBLOK_TOKEN,
+		components: {
+			page: "storyblok/Page",
+			header: "storyblok/Header",
+			main: "storyblok/Main",
+			navigation: "storyblok/Navigation",
+			hero: "storyblok/Hero",
+			banner: "storyblok/Banner",
+			table: "storyblok/Table",
+			column: "storyblok/Column",
+			cell: "storyblok/Cell",
+			text: "storyblok/Text",
+		},
     }), partytown(), sitemap(), compress()],
+	vite: {
+		plugins: [basicSsl()],
+		server: {
+			https: true,
+		},
+	},
 });
